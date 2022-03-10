325bb6b9eca93e5bb6c37583efd6ceda6d925f55
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2022-03-10 15:08:08 +0700

    design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,115 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">Hello World!</h1>
+    <div className="trello-trieuninhhan-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        <div className="column">
+          <header className="">Brains storm</header>
+          <ul>
+            <li>
+              <img src="https://pbs.twimg.com/media/FJX-2WQVQAAEIII?format=jpg&name=medium" alt="trieuninhhan-alt-img" />
+              Title:TRIEUNINHHAN
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer className="">Add another card</footer>
+        </div>
+        
+      </div>
     </div>
   )
 }
