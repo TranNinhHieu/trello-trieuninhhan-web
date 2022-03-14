022552cfe99798edb59d2805d9767a2d2e1a6c65
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2022-03-14 19:15:19 +0700

    inital data

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,27 +1,22 @@
 import React from 'react';
 
 import './Column.scss'
-import Task from 'components/Task/Task'
-function Column(){
-    return(
-        <div className="column">
-        <header className="">Brains storm</header>
-        <ul className="task-list">
-        <Task/>
-        
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-          <li className="task-item">Add what you'd like to work on below</li>
-        </ul>
-        <footer className="">Add another card</footer>
-      </div>
-    )
+import Card from 'components/Card/Card'
+
+import {mapOrder} from 'utilities/sorts'
+function Column(props){
+  const { column } = props
+  const cards = mapOrder(column.cards,column.cardOrder,'id')
+
+  return(
+      <div className="column">
+      <header className="">{column.title}</header>
+      <ul className="card-list">
+        {cards.map((card, index) => <Card key={index} card={card} /> )}
+      </ul>
+      <footer className="">Add another card</footer>
+    </div>
+  )
 
 }
 
