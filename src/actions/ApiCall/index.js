c93499ec87f8f47582c51756e5f88ea25b756ef0
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2022-03-28 11:07:04 +0700

    combine web get full board

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -3,6 +3,5 @@ import {API_ROOT} from 'utilities/constants'
 
 export const fetBoardDetails = async (id) => {
     const request = await axios.get(`${API_ROOT}/v1/boards/${id}`)
-    console.log(request)
     return request.data
 }
