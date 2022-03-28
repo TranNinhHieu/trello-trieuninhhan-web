4cb0c6c987d223513aa404e6cd82e277a38627f6
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2022-03-28 17:53:04 +0700

    crud column and card

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -5,3 +5,19 @@ export const fetBoardDetails = async (id) => {
     const request = await axios.get(`${API_ROOT}/v1/boards/${id}`)
     return request.data
 }
+
+export const createNewColumn = async (data) => {
+    const request = await axios.post(`${API_ROOT}/v1/columns`,data)
+    return data
+}
+
+//update or remove column
+export const updateColumn = async (id, data) => {
+    const request = await axios.put(`${API_ROOT}/v1/columns/${id}`,data)
+    return data
+}
+
+export const createNewCard = async (data) => {
+    const request = await axios.post(`${API_ROOT}/v1/cards`,data)
+    return data
+}
