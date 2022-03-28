373aefc1388c22b3847c23875d019e1c6c3f8052
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2022-03-28 23:03:15 +0700

    Deploy to firebase.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,3 @@
-export const API_ROOT = 'http://localhost:8880'
+export const API_ROOT = 'https://trello-dev-api.herokuapp.com'
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_COFIRM = 'MODAL_ACTION_COFIRM'
